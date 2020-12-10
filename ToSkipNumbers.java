ca7491a5ff00b40faa7b3d0a452464364e3e10e3
Supritha A <75771783+Supritha11@users.noreply.github.com>
2020-12-10 12:09:42 +0530

    ToSkipMultipleOf5

---
+++ ToSkipNumbers.java
@@ -0,0 +1,20 @@
+
+public class ToSkipNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("To skip multiple of 5");
+int i=1,n=5;
+while(i<=10)
+{
+	if(i%n==0)
+	{
+i++;
+	continue;
+}
+System.out.println(i);
+i++;
+}
+	
+}
+	}
