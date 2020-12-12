7c41d2d5d1cb70224355b105b90ebc1262da23d4
Supritha A <75771783+Supritha11@users.noreply.github.com>
2020-12-12 09:06:53 +0530

    AssignmentOperators

---
+++ AssignmentOperators.java
@@ -0,0 +1,19 @@
+class AssignmentOperators{
+public static void main(String args[]){
+int a=10;
+int b =5;
+int c;
+c = a + b;
+System.out.println("c=a+b=:"+c);
+c+=a;
+System.out.println("c +=a =:"+c);
+c-=a;
+System.out.println("c -=a =:"+c);
+c*=a;
+System.out.println("c *=a = :"+c);
+c/=a;
+System.out.println("c /=a = :"+c);
+c%=a;
+System.out.println("c %=a = :"+c);
+}
+}
