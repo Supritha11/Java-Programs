3ed8b5f4eda9080ec684c168b8b3f3151498fb41
Supritha A <75771783+Supritha11@users.noreply.github.com>
2020-12-12 08:36:15 +0530

    ProgramToSwapTwoNumbers

---
+++ ProgramToSwapTwoNumbers.java
@@ -0,0 +1,18 @@
+
+public class ProgramToSwapTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b = 30, temp;
+		System.out.println("Before swapping ");
+		System.out.println("Value of a is :" + a);
+		System.out.println("Value of b is :" + b);
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("After swapping ");
+		System.out.println("Value of a is :" + a);
+		System.out.println("Value of b is :" + b);
+	}
+
+}
