5779694770e053c81cd69bf52f6f3539d5ba6c09
Supritha A <75771783+Supritha11@users.noreply.github.com>
2020-12-17 16:40:31 +0530

    initializationblockexample

---
+++ TelevisionTester.java
@@ -0,0 +1,19 @@
+package com.xworkz.block;
+
+public class TelevisionTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Television lg = new Television();
+       System.out.println("Brand : "+lg.brand);
+       System.out.println("Price : "+lg.price);
+       System.out.println("Size in Inches : "+lg.sizeininches);
+       System.out.println("----------------------------------------------------");
+       Television mi = new Television("MI",35000.00,50);
+       System.out.println("Brand : "+mi.brand);
+       System.out.println("Price : "+mi.price);
+       System.out.println("Size in Inches : "+mi.sizeininches);
+      
+	}
+
+}
